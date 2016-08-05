7041cdd11bc0a499c1e8625e6bf5698634a8c767
IvanaG1t <ivana2908@gmail.com>
2016-08-05 14:01:02 +0200

    adding report_4

---
+++ code/ivana/server.py
@@ -0,0 +1,24 @@
+print "hello PW"
+
+x=1
+if x==1:
+    print "voila!"
+    print "toto"
+
+
+y="test"
+z="tet"
+
+print y+z #this brigs them together: testtet
+
+xx= ["t", "a"]
+
+print x
+print xx[0]
+
+x.append("r")
+print x
+
+xy = {"a" : 1, "b" : 2}
+print xy["1"]
+
