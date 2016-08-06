75f23545457328f01704196b427f562c1ca9d7df
Keoma Brun <keoma.brun@inria.fr>
2016-08-06 09:23:34 +0200

    adding web server plot example

---
+++ code/python/web_server_example.py
@@ -0,0 +1,27 @@
+from bottle import route, run, static_file, template
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+@route('/<filename>')
+def hello(filename):
+    #return "Hello World!"
+    return static_file(filename,".")
+
+@route('/plot/<name>')
+def graph(name):
+    df = pd.read_csv("test.csv")
+    plt.plot(df.index,df.val)
+    plt.savefig('graphs.png')
+
+    return template(
+        '''<h1>Congrats!</h1>
+        <div>
+          <img src="../graphs.png">
+        </div>
+        '''
+    )
+
+run(host='localhost', port=8080, debug=True)
+
